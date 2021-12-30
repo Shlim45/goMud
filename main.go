fdecbb10b26d459b6ea23c86d3e2f8ed5b6cd4c3
Jon Hawranko <jhawranko@gmail.com>
2021-12-29 22:41:29 -0500

    Receive from and send messages to client

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 func handleConnection(conn net.Conn) error {
 	log.Println("Connection accepted")
+
+	buf := make([]byte, 4096)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Fatalln("Connection ended by client", err)
+			return err
+		}
+		if n == 0 {
+			log.Println("Closing connection")
+			break
+		}
+		msg := strings.TrimSpace(string(buf[0 : n-2]))
+		log.Println("Received message:", msg)
+
+		resp := fmt.Sprintf("You said \"%s\"\r\n", msg)
+		n, err = conn.Write([]byte(resp))
+		if err != nil {
+			log.Fatalln("Error writing to connection", err)
+			return err
+		}
+		if n == 0 {
+			log.Println("Closing connection")
+			break
+		}
+	}
+
 	return nil
 }
 
